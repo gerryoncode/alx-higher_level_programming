0da98410e9b8817b1950e56ac99d42dfce9605e0
Gerald <gerryncode@gmail.com>
2023-12-19 21:38:30 -0800

    f/0-select_states.py:Fixed the mysql issue

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Lists all states from the databse hbtn_0e_0_usa """
+# host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306 )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows= cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
