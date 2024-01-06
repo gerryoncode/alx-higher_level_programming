717399bf722746ed6730ab6d471fb5d2b03ef932
Gerald <gerryncode@gmail.com>
2024-01-06 00:14:01 -0800

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send a POST request with contents of a file
-curl -X POST "$1" -H "Content-Type: application/json" -d "@$2"
+curl -X POST "$1" -H "Content-Type: application/json" -d "$(cat "$2")"
