dba345f090c7a0f8f4abca279c35c5516ef88d5e
Gerald <gerryncode@gmail.com>
2024-01-06 21:54:00 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Ascript that
-- fetches https://alx-intranet.hbtn.io/status
-- uses urllib package
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
+
 
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
-    
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
