de000c71494a8aed0e3cbeb1211480cb65ba09cd
Gerald <gerryncode@gmail.com>
2024-01-07 07:01:03 -0800

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that
+-takes in a url and an email address
+-sends a post request to the passed url with the email as a parameter
+-displays the body of the response
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    r = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    print(r)
