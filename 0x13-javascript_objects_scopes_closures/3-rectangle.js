79409983a4b36243a21ee828535783381d4d6969
Gerald Mbuthia <gerryoncode@gmail.com>
2024-06-18 13:35:18 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
     }
-    print(){
-        let rec = '';
-        for(let i = 0; i < this.height; i++) {
-           for(let j = 0; j < this.width; j++) {
-                rec += 'x';
-           }
-            console.log(rec);
-            rec = '';
-        }
+  }
+
+  print () {
+    let rec = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rec += 'x';
+      }
+      console.log(rec);
+      rec = '';
     }
+  }
 }
 
 module.exports = Rectangle;
