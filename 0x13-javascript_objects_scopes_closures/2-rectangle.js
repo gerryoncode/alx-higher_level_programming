aa14b04da556629669e9683b85a3bb3a8c09d1ca
Gerald Mbuthia <gerryoncode@gmail.com>
2024-06-18 13:02:38 -0700

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-  constructor (w, h) {
-    if (w <= 0 && h <= 0) {
-      return {};
+    constructor(w, h) {
+        if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+            this.width = w;
+            this.height = h;
+        }
     }
-    this.width = w;
-    this.height = h;
-  }
 }
 
 module.exports = Rectangle;
