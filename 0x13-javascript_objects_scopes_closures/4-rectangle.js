4fc973d9bedb8ebdd37a789a4e346a978d0d1769
Gerald Mbuthia <gerryoncode@gmail.com>
2024-06-18 13:57:53 -0700

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
     let rec = '';
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        rec += 'x';
+        rec += 'X';
       }
       console.log(rec);
       rec = '';
