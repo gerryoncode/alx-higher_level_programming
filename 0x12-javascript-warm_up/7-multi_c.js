0620751400a9b0a37cb0dd9a384223ebaa258b81
Gerald Mbuthia <gerryoncode@gmail.com>
2024-06-17 22:11:14 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
-#!/usr/bin/env node
-
-if (isNaN(process.argv[2])) {
+#!/usr/bin/node
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
